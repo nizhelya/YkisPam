52a527a87dd20173da8ef0b4ddfce2683f276347
Rodion <rshulik74@gmail.com>
2025-01-03 02:16:31 +0200

    enhance AddApartmentScreen.kt

--- app/src/main/java/com/ykis/mob/ui/screens/appartment/AddApartmentScreen.kt
+++ app/src/main/java/com/ykis/mob/ui/screens/appartment/AddApartmentScreen.kt
@@ -1,15 +1,26 @@
 package com.ykis.mob.ui.screens.appartment
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
@@ -19,9 +30,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -29,11 +45,124 @@ import com.ykis.mob.R
 import com.ykis.mob.core.composable.BasicField
 import com.ykis.mob.core.composable.BasicImageButton
 import com.ykis.mob.ui.components.appbars.AddAppBar
+import com.ykis.mob.ui.components.appbars.DefaultAppBar
 import com.ykis.mob.ui.navigation.NavigationType
 import com.ykis.mob.ui.navigation.navigateToInfoApartment
+import com.ykis.mob.ui.theme.YkisPAMTheme
 import com.ykis.mob.R.string as AppText
 
 
+@Composable
+fun AddApartmentScreenStateless(
+    modifier: Modifier = Modifier,
+    isButtonEnabled : Boolean,
+    onDrawerClicked: () -> Unit,
+    onAddClick : () -> Unit,
+    navigationType: NavigationType,
+    code : String,
+    onCodeChanged : (String)-> Unit
+                                ) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DefaultAppBar(
+                navigationType = navigationType,
+                onDrawerClick = onDrawerClicked,
+                title = stringResource(id = R.string.add_appartment),
+                canNavigateBack = false
+            )
+            Column(
+                modifier = modifier
+                    .widthIn(max = 460.dp)
+                    .padding(horizontal = 16.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Card(
+                    modifier = modifier,
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+                ) {
+                        Column(
+                            modifier = Modifier
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center,
+                                modifier = Modifier.padding(PaddingValues(8.dp)),
+
+                                ) {
+                                Column(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                ) {
+
+                                    Text(
+                                        text = stringResource(id = R.string.tooltip_code),
+                                        modifier = Modifier.padding(4.dp),
+                                        textAlign = TextAlign.Left,
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                }
+                            }
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center,
+                                modifier = Modifier.padding(PaddingValues(8.dp)),
+
+                                ) {
+                                Column(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .padding(start = 4.dp, end = 8.dp)
+                                ) {
+                                    OutlinedTextField(
+                                        value = code,
+                                        onValueChange = { newText ->
+                                            val filteredText = newText.filter { it.isDigit() }
+                                            onCodeChanged(filteredText)
+                                        },
+                                        label = {
+                                            Text(
+                                                text = stringResource(id = R.string.secret_сode)
+                                            )
+                                        },
+                                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                                    )
+
+                                }
+                                Button(
+                                    onClick = { onAddClick() },
+                                    enabled = isButtonEnabled,
+                                    colors = ButtonDefaults.buttonColors().copy(
+                                         disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                    )
+                                )
+                                {
+                                    Row(
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                                    ) {
+                                        Image(
+                                            painter = painterResource(R.drawable.ic_stat_name),
+                                            contentDescription = null,
+                                            colorFilter = ColorFilter.tint(if(isButtonEnabled) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSurfaceVariant)
+                                        )
+                                        Text(text = stringResource(id = R.string.add))
+                                    }
+                                }
+                            }
+                        }
+                }
+
+            }
+    }
+}
 @Composable
 fun AddApartmentScreenContent(
     modifier: Modifier = Modifier,
@@ -52,101 +181,40 @@ fun AddApartmentScreenContent(
         }
     }
     val keyboard = LocalSoftwareKeyboardController.current
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        AddAppBar(
-            modifier = Modifier,
-            stringResource(id = R.string.add_flat_secret_сode),
-            stringResource(id = R.string.add_appartment),
-            onBackPressed = {
-                navController.popBackStack()
-                viewModel.onSecretCodeChange("")
-            },
-            canNavigateBack = canNavigateBack,
-            onDrawerClicked = onDrawerClicked,
-            navigationType = navigationType
-        )
-        Card(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp, horizontal = 12.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(48.dp),
-                contentColor = MaterialTheme.colorScheme.onSecondaryContainer
-            )
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier.padding(PaddingValues(8.dp)),
 
-                        ) {
-                        Column(
-                            modifier = Modifier
-                                .weight(1f)
-                        ) {
-
-                            Text(
-                                text = stringResource(id = R.string.tooltip_code),
-                                modifier = Modifier.padding(4.dp),
-                                textAlign = TextAlign.Left,
-                                style = MaterialTheme.typography.bodyLarge
-                            )
-                        }
-                    }
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier.padding(PaddingValues(8.dp)),
-
-                        ) {
-                        Column(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(start = 4.dp, end = 8.dp)
-                        ) {
-                            BasicField(
-                                AppText.secret_сode,
-                                AppText.secret_сode,
-                                secretCode,
-                                onNewValue = { viewModel.onSecretCodeChange(it)},
-                                modifier = Modifier.padding(4.dp),
-                            )
-                        }
-                        BasicImageButton(
-                            AppText.add,
-                            R.drawable.ic_stat_name,
-                            modifier = Modifier,
-                            enabled = buttonEnabled
-                        )
-                        {
-                            keyboard?.hide()
-                            viewModel.addApartment {
-                                closeContentDetail()
-                                navController.navigateToInfoApartment()
-                            }
-                        }
-                    }
-                }
+    AddApartmentScreenStateless(
+        isButtonEnabled = buttonEnabled,
+        onDrawerClicked = onDrawerClicked,
+        onAddClick = {
+            keyboard?.hide()
+            viewModel.addApartment {
+                closeContentDetail()
+                navController.navigateToInfoApartment()
             }
+        },
+        navigationType = navigationType,
+        code = secretCode,
+        onCodeChanged = {
+            viewModel.onSecretCodeChange(it)
         }
 
-    }
+    )
 }
 
 
-
+@Preview
+@Composable
+private fun AddApartmentPreview() {
+    YkisPAMTheme {
+        Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)){
+            AddApartmentScreenStateless(
+                isButtonEnabled = true,
+                onDrawerClicked = {},
+                onAddClick = {},
+                navigationType = NavigationType.BOTTOM_NAVIGATION,
+                code = "",
+                onCodeChanged = {}
+            )
+        }
+    }
+}
